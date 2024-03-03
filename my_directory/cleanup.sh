e3514410a0ced9b6320249c65a13cb23481308dc
slhartmann42 <157656737+slhartmann42@users.noreply.github.com>
2024-03-02 23:24:20 -0600

    Create cleanup.sh
    begin cleanup.sh

---
+++ my_directory/cleanup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#Variables
+#totatl_files_moved=0
+#total_bytes_moved=0
+#images_moved=0
+#image_bytes_0
+#documents_moved=0
+#document_bytes=0
+#pfs_moved=0
+#pbf_bytes=0
+#executables_moved=0
+#executable_bytes=0
+#data_files_moved=0
+#data_file_bytes=0
+
+
+#directory_checks
+dnm="images"
+if [[ -d /$dnm ]]; then
+	echo "The directory $dnm exists."
+else
+	mkdir $dnm
+	echo "Directory for $dnm created."
+fi
+
+#documents
+dnm="documents"
+if [[ -d /$dnm ]]; then
+	echo "The directory $dnm exists."
+else
+	mkdir $dnm
+	echo "Directory for $dnm exists."
+fi
+
+dnm="pdfs"
+if [[ -d /$dnm ]]; then
+	echo "The directory $dnm exists."
+else
+	mkdir $dnm
+	echo "The directory $dnm exists."
+fi
+
+dnm="executables"
+if [[ -d /$dnm ]]; then
+	echo "The directory $dnm exists."
+else
+	mkdir $dnm
+	echo "The directory $dnm exists."
+fi
+
+dnm="data"
+if [[ -d /$dnm ]]; then
+	echo "The directory $dnm exists."
+else
+	mkdir $dnm
+	echo "The directory $dnm exists."
+fi
+
+dnm="unknown"
+if [[ -d /$dnm ]]; then
+	echo "The directory $dnm exists."
+else
+	mkdir $dnm
+	echo "The directory $dnm exists."
+fi
+
