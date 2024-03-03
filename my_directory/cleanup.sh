3f853e4e08fc474a0c1414958331f5d27ead1f62
slhartmann42 <157656737+slhartmann42@users.noreply.github.com>
2024-03-03 17:21:57 -0600

    Update cleanup.sh

--- my_directory/cleanup.sh
+++ my_directory/cleanup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Variables
-#totatl_files_moved=0
-#total_bytes_moved=0
-#images_moved=0
-#image_bytes_0
-#documents_moved=0
-#document_bytes=0
-#pfs_moved=0
-#pbf_bytes=0
-#executables_moved=0
-#executable_bytes=0
-#data_files_moved=0
-#data_file_bytes=0
+total_files_moved=0
+total_bytes_moved=0
+images_moved=0
+image_bytes_0
+documents_moved=0
+document_bytes=0
+pfs_moved=0
+pbf_bytes=0
+executables_moved=0
+executable_bytes=0
+data_files_moved=0
+data_file_bytes=0
 
 #directory_checks
 dnm="images"
@@ -57,3 +57,18 @@ else
 	mkdir $dnm; echo "The directory $dnm was created."
 fi
 
+for file in *; do
+	if [ -x "$file" ]; then
+		((total_files_moved++))
+		((executables_moved++))
+		filesize="$(stat -c%s "$file")"
+		((total_bytes_moved+=filesize))
+		((executable_bytes+=filesize))
+		mv "$file" executables/$file
+	#else 
+		#choice=
+	fi
+done
+
+echo "Total files moved: $total_files_moved"
+echo "Total file bytes moved: $total_bytes_moved"
